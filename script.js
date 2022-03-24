0df0874c601110aee607a608a49e04f7811c30ad
emperor <tardigradedb@gmail.com>
2022-03-24 14:35:35 +0100

    book constructor

--- script.js
+++ script.js
@@ -1,21 +1,18 @@
-let head = {
-    glasses: 1
-  };
-  
-  let table = {
-    __proto__: head,
-    pen: 3
-  };
-  
-  let bed = {
-    __proto__: table,
-    sheet: 1,
-    pillow: 2
-  };
-  
-  let pockets = {
-    __proto__: bed,
-    money: 2000
-  };
+function book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
 
-console.log(pockets.pen)
+    this.info = function() {
+        if (read == true) {
+            isRead = "has been read."
+        } else {
+            isRead = "has not been read."
+        }
+        return `${title} by ${author}, ${pages} pages, ${isRead}`
+    }
+}
+
+const theHobbit = new book("The Hobbit", "J.R.R. Tolkien", 295, false)
+console.log(theHobbit.info())
